6cc6737b54fb387784a261eb8c2f86b108fcd692
Steven Myhre <smyhre@nwalarm.com>
2025-10-08 20:53:29 -0700

    IP

--- syncPbx.sh
+++ syncPbx.sh
@@ -8,8 +8,8 @@ case "$PBX" in
         cp /etc/asterisk/avaya_x.conf.pending /etc/asterisk/avaya_x.conf
         ;;
     "SVRPBX01")
-        scp /etc/asterisk/avaya_x.conf.pending SVRPBX01:/etc/asterisk/avaya_x.conf.pending
-        ssh SVRPBX01 "cp /etc/asterisk/avaya_x.conf /etc/asterisk/avaya_x.conf.bak-$(date +%Y%m%d%H%M%S) && cp /etc/asterisk/avaya_x.conf.pending /etc/asterisk/avaya_x.conf"
+        scp /etc/asterisk/avaya_x.conf.pending 192.168.1.105:/etc/asterisk/avaya_x.conf.pending
+        ssh 192.168.1.105 "cp /etc/asterisk/avaya_x.conf /etc/asterisk/avaya_x.conf.bak-$(date +%Y%m%d%H%M%S) && cp /etc/asterisk/avaya_x.conf.pending /etc/asterisk/avaya_x.conf"
         ;;
     *)
         echo "Usage: $0 {SVRPBX01|SVRPBX02}"
